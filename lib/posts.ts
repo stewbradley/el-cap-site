ce8dcd279c114ddd9ec87a447539758a9c31563e
Lazlo Benny <stewbradley@gmail.com>
2023-04-25 15:47:00 -0600

    more type fixes

--- lib/posts.ts
+++ lib/posts.ts
@@ -10,32 +10,33 @@ export function getSortedPostsData() {
     // Get file names under /posts
     const fileNames = fs.readdirSync(postsDirectory);
     const allPostsData = fileNames.map((fileName) => {
-        // remove ".md" from file name to get id
-        const id = fileName.replace(/\.md$/, '');
-
-        // Read markdown file as string
-        const fullPath = path.join(postsDirectory, fileName);
-        const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-        // Use gray-matter to parse the post metadata section
-        const matterResult = matter(fileContents);
-
-        // Combine the data with the id
-        return {
-            id,
-            date: matterResult.data.date,
-            ...matterResult.data,
-        };
+      // remove ".md" from file name to get id
+      const id = fileName.replace(/\.md$/, '');
+  
+      // Read markdown file as string
+      const fullPath = path.join(postsDirectory, fileName);
+      const fileContents = fs.readFileSync(fullPath, 'utf8');
+  
+      // Use gray-matter to parse the post metadata section
+      const matterResult = matter(fileContents);
+  
+      // Combine the data with the id and date
+      return {
+        id,
+        date: matterResult.data.date,
+        ...matterResult.data,
+      };
     });
+  
     // Sort posts by date
     return allPostsData.sort((a, b) => {
-        if (a.date < b.date) {
-            return 1;
-        } else {
-            return -1;
-        }
+      if (a.date < b.date) {
+        return 1;
+      } else {
+        return -1;
+      }
     });
-}
+  }
 
 export function getAllPostIds() {
     const fileNames = fs.readdirSync(postsDirectory);
@@ -62,7 +63,7 @@ export function getAllPostIds() {
     });
 }
 
-export async function getPostData(id) {
+export async function getPostData(id: string) {
     const fullPath = path.join(postsDirectory, `${id}.md`);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
   
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -1,24 +1,22 @@
 import Head from 'next/head';
-import PostLayout from '../components/layout/posts-index';
 import Link from 'next/link';
+import { motion } from 'framer-motion';
+import Balancer from 'react-wrap-balancer';
+import Image from 'next/image';
+import localFont from '@next/font/local';
+import PostLayout from '../components/layout/posts-index';
 import Date from '../components/date';
 import { getSortedPostsData } from '../lib/posts';
-import Balancer from 'react-wrap-balancer';
-import Card from "@/components/home/card";
-import Layout from "@/components/layout";
-import { motion } from "framer-motion";
-import { DEPLOY_URL, FADE_DOWN_ANIMATION_VARIANTS } from "@/lib/constants";
-import { Github, Twitter } from "@/components/shared/icons";
-import WebVitals from "@/components/home/web-vitals";
-import ComponentGrid from "@/components/home/component-grid";
-import Image from "next/image";
-import localFont from "@next/font/local";
+import ComponentGrid from '@/components/home/component-grid';
+import Card from '@/components/home/card';
+import WebVitals from '@/components/home/web-vitals';
+import { DEPLOY_URL, FADE_DOWN_ANIMATION_VARIANTS } from '@/lib/constants';
+import { Github, Twitter } from '@/components/shared/icons';
 
 const supply = localFont({
-  src: "../styles/Supply-Regular.otf",
-  variable: "--font-supply",
+  src: '../styles/Supply-Regular.otf',
+  variable: '--font-supply',
 });
-
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -30,11 +28,29 @@ export async function getStaticProps() {
   };
 }
 
+function PostList({ posts }) {
+  return (
+    <ul>
+      {posts.map(({ id, date, title }) => (
+        <li className="text-black" key={id}>
+          <Link href={`/posts_id/${id}`}>{title}</Link>
+          <br />
+          <small className="font-light font-sans">
+            <Date dateString={date} />
+          </small>
+          <hr className="h-px my-5 bg-gray-800 border-0 dark:bg-gray-700"></hr>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
 export default function Writing({ allPostsData }) {
   console.log('allPostsData is ', allPostsData);
+
   return (
     <PostLayout>
-        <motion.div
+      <motion.div
         className="max-w-xl px-5 xl:px-0"
         initial="hidden"
         whileInView="show"
@@ -50,35 +66,19 @@ export default function Writing({ allPostsData }) {
         }}
       >
         <motion.h1
-          className="text-black bg-clip-text text-left text-4xl font-bold tracking-[-0.02em] text-transparent drop-shadow-sm "
+          className="text-black bg-clip-text text-left text-4xl font-bold tracking-[-0.02em] text-transparent"
           variants={FADE_DOWN_ANIMATION_VARIANTS}
         >
-          <Balancer className="font-supply text-slate-800"> el cap writing</Balancer>
+          <Balancer className="font-supply text-slate-800">el cap writing</Balancer>
         </motion.h1>
-        <motion.p
-          className="mt-6 text-center text-black md:text-xl"
-          variants={FADE_DOWN_ANIMATION_VARIANTS}
-        >
-         <Balancer>
-            <section>
-                <ul className="text-left font-supply">
-                    {allPostsData.map(({ id, date, title }) => (
-                        <li className="text-black" key={id}>
-                            <Link href={`/posts_id/${id}`}>{title}</Link>
-                            <br />
-                            <small className="font-light font-sans">
-                                <Date dateString={date} />
-                            </small>
-                            <hr className="h-px my-5 bg-gray-800 border-0 dark:bg-gray-700"></hr>
-                        </li>
-                    ))}
-                </ul>
-        </section>
-      </Balancer> 
+        <motion.p className="mt-6 text-left text-black md:text-xl" variants={FADE_DOWN_ANIMATION_VARIANTS}>
+          <Balancer>
+            <section key="post-list">
+              <PostList posts={allPostsData} />
+            </section>
+          </Balancer>
         </motion.p>
-        
       </motion.div>
-        
     </PostLayout>
   );
 }
